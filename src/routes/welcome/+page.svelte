81f74d2f4686dc2faa3e82bbf5daa70ccc337850
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-27 03:53:12 +0700

    feat: configure darkmode

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Icon from '@iconify/svelte';
 	import { Button } from 'flowbite-svelte';
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<p>Welcome to ThePortfolio</p>
+<p class=" text-foreground-900">Welcome to ThePortfolio</p>
 
-<Icon icon="mdi-light:home" />
+<Icon icon="mdi-light:home" class=" text-foreground-900" />
 <Button>Default</Button>
+<DarkMode />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 import flowbitePlugin from 'flowbite/plugin';
 
+const generateColor = (name, colors) => {
+	return colors.reduce((obj, num) => ({ ...obj, [num]: `var(--${name}-${num})` }), {});
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -10,7 +14,6 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				// flowbite-svelte
 				primary: {
 					50: '#FFF5F2',
 					100: '#FFF1EE',
@@ -22,6 +25,22 @@ export default {
 					700: '#EB4F27',
 					800: '#CC4522',
 					900: '#A5371B'
+				},
+				foreground: generateColor(
+					'foreground',
+					[50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
+				),
+				slate: {
+					50: '#f8fafc',
+					100: '#f1f5f9',
+					200: '#e2e8f0',
+					300: '#cbd5e1',
+					400: '#94a3b8',
+					500: '#64748b',
+					600: '#475569',
+					700: '#334155',
+					800: '#1e293b',
+					900: '#0f172a'
 				}
 			}
 		}
