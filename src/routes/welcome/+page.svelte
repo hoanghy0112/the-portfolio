af86a1a845207bc6d9220dc66ae5855ac18df070
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-27 03:16:30 +0700

    feat: configure tailwind and flowbite 🎨

---
+++ src/routes/welcome/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Icon from '@iconify/svelte';
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<p>Welcome to ThePortfolio</p>
+
+<Icon icon="mdi-light:home" />
+<Button>Default</Button>
