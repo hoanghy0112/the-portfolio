45a552d04e3afab645ed5d662a2344b5410a222b
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-27 11:20:12 +0700

    feat: add empty PortfolioItem component

---
+++ src/lib/components/PortfolioItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Portfolio } from '@prisma/client';
+	import { Card } from 'flowbite-svelte';
+
+	type Props = { portfolio: Portfolio };
+
+	const { portfolio }: Props = $props();
+</script>
+
+<Card href="/cards">
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		Noteworthy technology acquisitions 2021
+	</h5>
+	<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+		Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological
+		order.
+	</p>
+</Card>
