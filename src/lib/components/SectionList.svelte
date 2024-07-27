d04741f40f4447750fcd4375f30b3696077f09a6
Hoang Hy <hoanghy01122003@gmail.com>
2024-07-27 11:16:31 +0700

    feat: add SectionList component

---
+++ src/lib/components/SectionList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		title: string;
+		children?: Snippet;
+	};
+
+	const { title, children }: Props = $props();
+</script>
+
+<div class=" flex flex-col gap-4">
+	<h2 class=" font-semibold text-2xl">{title}</h2>
+	<div class=" w-full overflow-auto flex flex-row gap-4">
+		{@render children?.()}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <script>
+	import SectionList from '$lib/components/SectionList.svelte';
 </script>
+
+<div>
+	<SectionList title="Your Porfolio"></SectionList>
+</div>
