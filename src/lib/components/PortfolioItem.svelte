7f68d6c649b2ece4e1f5243c3a0b9e93a6c8e581
Hoang Hy <hoanghy01122003@gmail.com>
2024-08-02 03:08:12 +0700

    fix

--- src/lib/components/PortfolioItem.svelte
+++ src/lib/components/PortfolioItem.svelte
@@ -6,6 +6,7 @@
 	import { Button, Carousel } from 'flowbite-svelte';
 	import { scale } from 'svelte/transition';
 	import Link from './Link.svelte';
+	import { timeDiffString } from '$lib/utils/time-manipulation';
 
 	type Props = { portfolio: Portfolio };
 
@@ -15,16 +16,20 @@
 </script>
 
 <div
-	class="group w-full flex flex-col {isDeleting ? 'blur-md' : 'blur-0'} duration-500"
+	class={`group w-full flex flex-col ${isDeleting ? 'blur-md' : 'blur-0'} duration-500`}
 	out:scale={{ duration: 300, start: 0.2 }}
 >
 	<div
-		class=" w-full rounded-xl overflow-hidden flex flex-col bg-white dark:bg-foreground-100 shadow-lg dark:border-[0.5px] dark:border-foreground-400 duration-[600ms]"
+		class=" w-full px-4 py-6 rounded-xl overflow-hidden flex flex-col gap-10 bg-white dark:bg-foreground-100 shadow-lg dark:border-[0.5px] dark:border-foreground-400 duration-[600ms]"
 	>
-		<div class=" h-full bg-white dark:bg-foreground-100 flex flex-col gap-2 px-4 py-6">
+		<div class=" h-full bg-white dark:bg-foreground-100 flex flex-col gap-2">
 			<Link href="/portfolio/{portfolio.id}" class=" text-foreground-900 text-lg font-semibold">
 				{portfolio.name || 'Untitle portfolio'}
 			</Link>
+			<p class=" text-foreground-600">Updated {timeDiffString(new Date(portfolio.updatedAt))}</p>
+		</div>
+		<div>
+			<iframe src="http://localhost:5173/" title="W3Schools Free Online Web Tutorials"></iframe>
 		</div>
 		<!-- <div
 			class=" back px-20 flex flex-col justify-center items-stretch gap-4 opacity-0 delay-[200ms] scale-105 z-20 rounded-xl group-hover:opacity-100 absolute w-full h-full top-0 left-0 bg-background-default"
